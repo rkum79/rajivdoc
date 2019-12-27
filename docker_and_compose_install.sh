63f3a1fab0e42e82b56bbb90987f94e30fa6df1e
Rajiv Kumar 4 <rkumar113@sapient.com>
2019-12-27 14:12:06 +0530

    Create docker_and_compose_install.sh

---
+++ docker_and_compose_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Alternatively you can use the official docker install script
+wget -qO- https://get.docker.com/ | sh
+
+# Install docker-compose
+VERSION=$(curl -s -o /dev/null -I -w "%{redirect_url}\n" https://github.com/docker/compose/releases/latest | grep -oP "[0-9]+(\.[0-9]+)+$")
+curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VER/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
