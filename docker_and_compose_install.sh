2d4ca950aa255777e430fdc6596785be76ae269d
Rajiv Kumar 4 <rkumar113@sapient.com>
2019-12-27 15:56:47 +0530

    Update docker_and_compose_install.sh

--- docker_and_compose_install.sh
+++ docker_and_compose_install.sh
@@ -7,5 +7,5 @@ set -e
 # Install docker-compose
 VERSION=$(curl -s -o /dev/null -I -w "%{redirect_url}\n" https://github.com/docker/compose/releases/latest | grep -oP "[0-9]+(\.[0-9]+)+$")
 curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VER/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+#chmod +x /usr/local/bin/docker-compose
+#docker-compose --version
