023b07ac6970412bd4e27cf90237f7f58ef5c798
Rajiv Kumar 4 <rkumar113@sapient.com>
2019-12-30 13:04:27 +0530

    Update docker_and_compose_install.sh

--- docker_and_compose_install.sh
+++ docker_and_compose_install.sh
@@ -3,7 +3,7 @@ sudo true
 set -eu
 
 # Alternatively you can use the official docker install script
-sudo wget -qO- https://get.docker.com/ | bash
+#sudo wget -qO- https://get.docker.com/ | bash
 sudo usermod -aG docker Simon
 # Install docker-compose
 COMPOSE_VER=$(curl -s -o /dev/null -I -w "%{redirect_url}\n" https://github.com/docker/compose/releases/latest | grep -oP "[0-9]+(\.[0-9]+)+$")
